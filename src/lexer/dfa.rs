eaae80df21b01f5bd1fae0a4a68922a65d5890c3
buchenglei <mybc724@163.com>
2015-11-25 20:13:34 +0800

    完成可识别数字（整形和浮点型）的DFA

--- src/lexer/dfa.rs
+++ src/lexer/dfa.rs
@@ -15,6 +15,9 @@ pub fn choose_dfa(c: char) -> Option<DFA> {
 	if c == ' ' {
 		return Some(dfa_whitespace)
 	}
+    if c.is_numeric() {
+        return Some(dfa_num)
+    }
 	
 	// 如果c不是以上任何一个类型的字符，则报错
 	panic!("Error: Unrecognized char!");
@@ -62,4 +65,41 @@ pub fn dfa_whitespace(s: u8, c: char) -> State {
 		}
 		_ =>  State::Unaccepted
 	}
+}
+
+// 匹配数字(整形和浮点型)的dfa
+pub fn dfa_num(s: u8, c: char) -> State {
+    match s {
+        0 => {
+            if c.is_numeric() {
+                State::MoveTo(1)
+            } else {
+				State::Unaccepted
+            }
+        },
+        1 => {
+            if c.is_numeric() {
+                State::MoveTo(1)
+            } else if c == '.' {
+                State::MoveTo(2)
+            } else {
+                State::Accepted(WordType::Value)
+            }
+        },
+        2 => {
+            if c.is_numeric() {
+                State::MoveTo(3)
+            } else {
+				State::Unaccepted
+            }
+        },
+        3 => {
+            if c.is_numeric() {
+                State::MoveTo(3)
+            } else {
+                State::Accepted(WordType::Value)
+            }
+        },
+		_ =>  State::Unaccepted
+    }
 }
