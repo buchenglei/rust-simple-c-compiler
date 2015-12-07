9f0da72b20711d7b5baaf1c5d7d73acc1be3ce58
buchenglei <mybc724@163.com>
2015-12-07 13:54:01 +0800

    添加从命令行读取参数作为文件名的代码

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use std::env;
 // 词法分析器模块
 mod lexer;
 
 fn main() {
-    let tokens = lexer::parse::run("d:/foo.c");
+    // 将获得的命令行参数作为编译的文件名
+    if env::args().len() != 2 {
+        panic!("Usage: ./myC filename.c");
+    }
+    let filename = match env::args().nth(1) {
+        Some(name) => name,
+        None => panic!("Get filename error!")
+    };
+    let tokens = lexer::parse::run(&filename[..]);
     for i in &tokens {
         println!("{:?}", i);
     }
